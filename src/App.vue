a78a4e32398eaee3d5d8b862d5654e236230eac8
codingChen <148864387@qq.com>
2024-09-18 17:07:36 +0800

    commit demo

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import TimeLine from "./components/TimeLine/src/index.vue";
+
+const timelineRef = ref();
+
+const zoomIndex = ref(0);
+
+onMounted(() => {
+  setInterval(() => {
+    timelineRef.value.setTime(Date.now());
+  }, 1000);
+});
+</script>
+
+<template>
+  <TimeLine
+    ref="timelineRef"
+    :init-time="Date.now()"
+    :init-zoom-index="zoomIndex"
+    style="height: 70px"></TimeLine>
+</template>
+
+<style lang="less"></style>
